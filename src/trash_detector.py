1db66b64a9fffe70fee44a346870deec4b0d6fdf
cheelam <tancheelam2@gmail.com>
2022-07-11 23:10:53 +0800

    Update trash_detector.py

--- src/trash_detector.py
+++ src/trash_detector.py
@@ -25,28 +25,39 @@ if __name__ == "__main__":
         # by frame
         ret, frame = vid.read()
 
+        # Input the image into the model to get prediction result
         pred = model(frame)
 
+        # Extracted all the predicted bounding boxes
         bboxes = pred.pandas().xyxy
 
+        # Convert pandas dataframe into dictionary
         dict_result = bboxes[0].to_dict()
 
+        # Obtained the number of predicting objects
         n_results = len(dict_result['name'])
 
         annotated_img = frame
 
         for i in range(n_results):
+            # Get the ith bbox start point
             start_point = (int(dict_result['xmin'][i]), int(dict_result['ymin'][i]))
+            # Calculate the ith font location
             font_point = (int(dict_result['xmin'][i]), int(dict_result['ymin'][i]) - 20)
+            # Get the ith bbox end point
             end_point = (int(dict_result['xmax'][i]), int(dict_result['ymax'][i]))
+            # Get the ith prediction result
             pred_class = dict_result['class'][i]
+            # Get the label mapping
             pred_label = labels[pred_class]
+            # Get the confidence score
             accuracy = dict_result['confidence'][i] * 100
 
             print("%s %.2f" % (pred_label, accuracy))
 
             pred_class += 1
 
+            # Do the color mappign according to the recyle bin color
             if pred_class in [12, 42, 74]: # Paper (Blue)
                 color = (255, 0, 0) 
                 pred_label = "paper" 
@@ -65,7 +76,9 @@ if __name__ == "__main__":
             thickness = 2
             font = font = cv2.FONT_HERSHEY_COMPLEX_SMALL
 
+            # Plot the bounding box
             cv2.rectangle(annotated_img, start_point, end_point, color, thickness)
+            # Write the text on image
             cv2.putText(annotated_img, label, font_point, font, 0.7, color)
     
         # Display the resulting frame
